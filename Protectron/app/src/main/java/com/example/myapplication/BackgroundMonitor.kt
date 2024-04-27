27c303e5d89756ca9ef4d79954e025e607ee6cd8
Francesco <70409297+Fr4nKB@users.noreply.github.com>
2024-04-27 14:22:20 +0200

    Initial commit
    Added bottom navbar, home fragment with a button to start service, memoryful settings fragment

---
+++ Protectron/app/src/main/java/com/example/myapplication/BackgroundMonitor.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class BackgroundMonitor : Service() {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val sharedPreferences = getSharedPreferences("BG_SERVICE", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("isServiceRunning", true)
+        editor.apply()
+        // your code here
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        val sharedPreferences = getSharedPreferences("BG_SERVICE", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("isServiceRunning", false)
+        editor.apply()
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        // TODO: Return the communication channel to the service.
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+}
