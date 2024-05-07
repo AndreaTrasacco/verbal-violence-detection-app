a19e6a2ee359b3582cc719ddf5c6269d247c4cc1
AndreaTrasacco <andrea1900@outlook.it>
2024-05-07 21:52:55 +0200

    Correct recording in wav format

--- app/src/main/java/it/unipi/masss/recordingservice/AudioModelManager.kt
+++ app/src/main/java/it/unipi/masss/recordingservice/AudioModelManager.kt
@@ -11,46 +11,18 @@ import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 object AudioModelManager{
     var model : AudioModel? = null
     private fun extractFeatures(path:String): FloatArray {
-        Log.d(
-            "AUDIO MODEL MANAGER",
-            "before getInstance"
-        )
         val py = Python.getInstance()
-        Log.d(
-            "AUDIO MODEL MANAGER",
-            "after getInstance"
-        )
         val module = py.getModule("script")
-        Log.d(
-            "AUDIO MODEL MANAGER",
-            "after getModule"
-        )
 
         val funCall = module["get_features"]
-        Log.d(
-            "AUDIO MODEL MANAGER",
-            "after module[\"get_features\"] " + funCall.toString()
-        )
         val result = funCall?.call(path)
-        Log.d(
-            "AUDIO MODEL MANAGER",
-            "after call"
-        )
         return result!!.toJava(FloatArray::class.java)
     }
     fun classify(recordingService: RecordingService, path: String): Boolean {
         if (! Python.isStarted()) {
             Python.start(AndroidPlatform(recordingService));
         }
-        Log.d(
-            "AUDIO MODEL MANAGER",
-            "before extract feature"
-        )
         val audioFeatures = extractFeatures(path)
-        Log.d(
-            "AUDIO MODEL MANAGER",
-            "after extract feature"
-        )
         //TODO cambiare logica di caricamento del modello
         if (model == null)
             model = AudioModel.newInstance(recordingService)
@@ -62,11 +34,6 @@ object AudioModelManager{
         // Runs model inference and gets result.
         val outputs = model!!.process(inputFeature0)
         val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
-
-        Log.d(
-            "AUDIO MODEL MANAGER",
-            "feature string: " + outputFeature0.contentToString()
-        )
 
         val maxIdx = outputFeature0.indices.maxBy { outputFeature0[it] } ?: -1
 
--- app/src/main/java/it/unipi/masss/recordingservice/AudioRecording.kt
+++ app/src/main/java/it/unipi/masss/recordingservice/AudioRecording.kt
@@ -1,14 +1,10 @@
 package it.unipi.masss.recordingservice
 
-import android.media.MediaRecorder
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
 import java.io.File
 import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 import java.util.Timer
 import java.util.TimerTask
 import java.util.concurrent.Executors
@@ -21,7 +17,10 @@ class AudioRecording(private val recordingService: RecordingService) {
     private val recorderTask: RecorderTask? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
         RecorderTask(recordingService)
     } else null
-    private val PERIOD: Long = 20000 // ms
+
+    companion object {
+        const val PERIOD: Long = 20000 // ms
+    }
 
     fun startAudioRecording() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -33,65 +32,55 @@ class AudioRecording(private val recordingService: RecordingService) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             recorderTask?.cancel()
         }
-        timer.cancel();
+        timer.cancel()
     }
 
     @RequiresApi(Build.VERSION_CODES.S)
     class RecorderTask(private val recordingService: RecordingService) : TimerTask() {
-        private var mediaRecorder: MediaRecorder? = null
-        private val AMPLITUDE_THRESHOLD: Double = 60.0
-        private val CHECK_AMPLITUDE_SECONDS: Long = 1
-        private val RECORDING_FOR_ML_SECONDS: Long = 10
+        private var wavRecorder: WavRecorder? = null
+
+        companion object {
+            const val AMPLITUDE_THRESHOLD: Int = 60
+            const val CHECK_AMPLITUDE_SECONDS: Long = 2
+            const val RECORDING_FOR_ML_SECONDS: Long = 10
+
+        }
 
         override fun cancel(): Boolean {
             val retValue = super.cancel()
-            mediaRecorder?.release()
-            mediaRecorder = null
+            wavRecorder?.stopRecording()
             return retValue
         }
 
         override fun run() {
             // Initialization of mediaRecorder object
-            if (mediaRecorder == null)
-                mediaRecorder = MediaRecorder(recordingService)
-            startMediaRecorder(recordingService.filesDir.absolutePath + "/temp.wav")
-            getAmplitudeDB(mediaRecorder!!) // Needed because in this way at the 2nd call we can get a value != -infty
+            if (wavRecorder == null)
+                wavRecorder = WavRecorder(recordingService)
+            wavRecorder?.startRecording("temp.wav", true)
+            wavRecorder?.maxAmplitudeDb // Needed because in this way at the 2nd call we can get a value != -infinity
 
             Executors.newSingleThreadScheduledExecutor().schedule(
                 {
-                    var amplitude = 0.0
-                    mediaRecorder?.apply {
-                        stop()
-                        amplitude = getAmplitudeDB(this)
-                        Log.d("RECORDER TASK", "Detected amplitude: $amplitude dB")
-                        reset()
-                    }
+                    val amplitude = wavRecorder?.maxAmplitudeDb!!
+                    Log.d(RecorderTask::class.java.name, "Detected amplitude: $amplitude dB")
+                    wavRecorder?.stopRecording()
                     if (amplitude > AMPLITUDE_THRESHOLD) {
-                        Log.d("RECORDER TASK", "Start recording for subsequent detection")
-                        val outputFile =
-                            recordingService.filesDir.absolutePath + "/recording" + getTimestamp() + ".wav"
-                        startMediaRecorder(outputFile)
+                        Log.d(RecorderTask::class.java.name, "Start recording for subsequent detection")
+                        val outputFile = "recording_" + System.currentTimeMillis() + ".wav"
+                        wavRecorder?.startRecording(outputFile, true)
                         Executors.newSingleThreadScheduledExecutor().schedule(
                             {
-                                mediaRecorder?.apply {
-                                    stop()
-                                    reset()
-                                }
-                                Log.d(
-                                    "RECORDER TASK",
-                                    "Before classify"
-                                )
-                                val violentRecording = AudioModelManager.classify(recordingService,
-                                    outputFile)
-                                Log.d(
-                                    "RECORDER TASK",
-                                    "classification result: " + violentRecording.toString()
+                                wavRecorder?.stopRecording()
+                                val violentRecording = AudioModelManager.classify(
+                                    recordingService,
+                                    recordingService.filesDir.path + '/' + outputFile
                                 )
                                 if (violentRecording) // TODO Is there the need of copying the audio?
                                     recordingService.stopRecording(true)
-                                else{
-                                    val fdelete = File(outputFile)
-                                    fdelete.delete()
+                                else {
+                                    val fileDelete =
+                                        File(recordingService.filesDir.path + '/' + outputFile)
+                                    fileDelete.delete() // TODO Check against IOException
                                 }
                             }, RECORDING_FOR_ML_SECONDS, TimeUnit.SECONDS
                         )
@@ -99,31 +88,5 @@ class AudioRecording(private val recordingService: RecordingService) {
                 }, CHECK_AMPLITUDE_SECONDS, TimeUnit.SECONDS
             )
         }
-
-        private fun startMediaRecorder(outputFile: String) {
-            mediaRecorder?.apply {
-                setAudioSource(MediaRecorder.AudioSource.VOICE_RECOGNITION)
-                setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP) // wav format
-                setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-                setOutputFile(outputFile)
-
-                try {
-                    prepare()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-                start()
-            }
-        }
-
-        private fun getAmplitudeDB(mediaRecorder: MediaRecorder): Double =
-            20 * log10(abs(mediaRecorder.maxAmplitude.toDouble()))
-
-        private fun getTimestamp(): String = SimpleDateFormat(
-            "yyyyMMdd_HHmmss",
-            Locale.getDefault()
-        ).format(
-            Date()
-        )
     }
 }
