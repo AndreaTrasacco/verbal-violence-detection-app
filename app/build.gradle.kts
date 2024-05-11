ad35f27e2dc3e687ff6068fffd83e8162a260da5
nicolosalti <nicolo.salti@hotmail.it>
2024-05-11 16:01:56 +0200

    added class to handle the message received from firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.chaquo.python")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -86,6 +87,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.2.0")
     implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
     implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
+    implementation("com.google.firebase:firebase-messaging:24.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
