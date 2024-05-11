7fab97a4f8c50a0e0e74ffa0b7baa1305e0fb717
Iacopo Canetta <80352950+Top-Down@users.noreply.github.com>
2024-05-11 15:36:37 +0200

    Update RecordingsFragment.kt

--- app/src/main/java/it/unipi/masss/ui/recordings/RecordingsFragment.kt
+++ app/src/main/java/it/unipi/masss/ui/recordings/RecordingsFragment.kt
@@ -1,40 +1,110 @@
 package it.unipi.masss.ui.recordings
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import it.unipi.masss.databinding.FragmentRecordingsBinding
+import it.unipi.masss.project.R
+import it.unipi.masss.project.databinding.FragmentRecordingsBinding
+import java.io.File
+import AudioItem
+
+import android.media.MediaPlayer
+import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class RecordingsFragment : Fragment() {
 
     private var _binding: FragmentRecordingsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private var mediaPlayer: MediaPlayer? = null
+    private var currentlyPlaying: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(RecordingsViewModel::class.java)
-
+        // Inflate the layout for this fragment
         _binding = FragmentRecordingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val audio_path = context?.filesDir?.path + "/"
+        Log.d("Recordings", "audiopath: $audio_path")
+
+
+        val directory = File(audio_path)
+        val audioListLayout = view.findViewById<LinearLayout>(R.id.audioList)
+        val scrollViewLayout = view.findViewById<ScrollView>(R.id.scrollPage)
+        val audioItems = mutableMapOf<String, AudioItem>()
+
+
+        // Get an array of files in the directory
+        val files = directory.listFiles()
+        Log.d("Recordings", "FILES: $files")
+
+        // Iterate over the files and print their names
+        files?.forEach { file ->
+            if (file.isFile && file.name.startsWith("recording_")) {
+                val audioItem = AudioItem(requireContext(), file.name, file.name)
+                audioListLayout.addView(audioItem)
+                audioListLayout.requestLayout()
+                scrollViewLayout.requestLayout()
+
+                audioItems[file.name] = audioItem
+
+                audioItem.findViewById<FloatingActionButton>(R.id.playButton).setOnClickListener {
+                    // If the audio is playing, stop it
+                    if (mediaPlayer?.isPlaying == true) {
+                        mediaPlayer?.stop()
+                        mediaPlayer?.release()
+                        mediaPlayer = null
+
+                        val playingAudioItem = audioItems[currentlyPlaying]
+                        val playingButton = playingAudioItem?.findViewById<FloatingActionButton>(R.id.playButton)
+                        playingButton?.setImageResource(android.R.drawable.ic_media_play)
+                    }
+                    if(currentlyPlaying != file.name){
+                        // Otherwise, start playing the audio
+                        mediaPlayer = MediaPlayer().apply {
+                            setDataSource(audio_path + file.name)
+                            prepare()
+                            start()
+
+                            setOnCompletionListener {
+                                val playingAudioItem = audioItems[currentlyPlaying]
+                                val playingButton = playingAudioItem?.findViewById<FloatingActionButton>(R.id.playButton)
+                                playingButton?.setImageResource(android.R.drawable.ic_media_play)
+                                currentlyPlaying = null
+                            }
+                        }
+                        currentlyPlaying = file.name
+
+                        Log.d("Recordings", "PEFFORZA")
+                        val my_id = file.name.hashCode()
+                        Log.d("Recordings", "id: $my_id")
+
+
+                        val currentAudioItem = audioItems[file.name]
+                        Log.d("Recordings", "currentAudioItem: $currentAudioItem")
+
+                        val playButton = currentAudioItem?.findViewById<FloatingActionButton>(R.id.playButton)
+
+                        Log.d("Recordings", "PLAYBUTTON : $playButton")
+                        playButton?.setImageResource(android.R.drawable.ic_media_pause)
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
