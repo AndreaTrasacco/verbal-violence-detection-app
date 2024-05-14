ddcb827b3db3fddb03698eeb14ec5d9b719bbc61
nicolosalti <nicolo.salti@hotmail.it>
2024-05-14 17:51:00 +0200

    changed logic for topicSubscription

--- app/src/main/java/it/unipi/masss/TopicSubscriber.kt
+++ app/src/main/java/it/unipi/masss/TopicSubscriber.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.ktx.messaging
 
+/*
 class TopicSubscriber : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (Intent.ACTION_BOOT_COMPLETED == intent.action ||
@@ -28,5 +29,29 @@ class TopicSubscriber : BroadcastReceiver() {
         }
         if (Intent.ACTION_SHUTDOWN == intent.action)
             Firebase.messaging.unsubscribeFromTopic("protectron")
+    }
+}
+
+ */
+object TopicSubscriber {
+    fun subscribeToTopic(context: Context) {
+        val sharedPreference = context.getSharedPreferences("SUBSCRIBED", Context.MODE_PRIVATE)
+        val subscribed = sharedPreference.getBoolean("subscribed", false)
+
+        if (subscribed == false) {
+            Firebase.messaging.subscribeToTopic("protectron")
+                .addOnCompleteListener { task ->
+                    var msg = "Subscribed"
+                    if (!task.isSuccessful) {
+                        msg = "Subscribe failed"
+                    }
+                    Log.d(TopicSubscriber::class.java.simpleName, msg)
+                }
+
+            val sharedPreference = context.getSharedPreferences("SUBSCRIBED", Context.MODE_PRIVATE)
+            val editor = sharedPreference.edit()
+            editor.putBoolean("subscribed", true)
+            editor.apply()
+        }
     }
 }
