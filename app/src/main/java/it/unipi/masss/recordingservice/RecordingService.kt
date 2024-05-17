8c368e7c96d39d017b35355a5a054b95c581105f
AndreaTrasacco <andrea1900@outlook.it>
2024-05-17 09:07:22 +0200

    Update RecordingService.kt

--- app/src/main/java/it/unipi/masss/recordingservice/RecordingService.kt
+++ app/src/main/java/it/unipi/masss/recordingservice/RecordingService.kt
@@ -29,7 +29,7 @@ class RecordingService : Service() {
     private lateinit var notificationBuilder: NotificationCompat.Builder
 
     companion object {
-        const val PERIOD: Long = 20000 // ms
+        const val CHECK_AMPLITUDE_PERIOD: Long = 20000 // ms
         private const val TAG = "RecordingService"
     }
 
@@ -65,7 +65,7 @@ class RecordingService : Service() {
         startForeground(ProtectronApplication.BG_NOTIF_ID, notificationBuilder.build())
         // Start the recording logic
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            timer.schedule(RecorderTask(this), 0, PERIOD)
+            timer.schedule(RecorderTask(this), 0, CHECK_AMPLITUDE_PERIOD)
         }
     }
 
@@ -89,8 +89,8 @@ class RecordingService : Service() {
         private var isCanceled = false
 
         companion object {
-            const val AMPLITUDE_THRESHOLD: Int = 40
-            const val CHECK_AMPLITUDE_SECONDS: Long = 2
+            const val AMPLITUDE_DB_THRESHOLD: Int = 55 // Decibels
+            const val CHECK_AMPLITUDE_DURATION: Long = 2 // Seconds
             const val RECORDING_FOR_ML_SECONDS: Long = 10
             private const val TAG = "RecorderTask"
         }
@@ -113,7 +113,7 @@ class RecordingService : Service() {
                     val amplitude = wavRecorder?.maxAmplitudeDb!!
                     Log.d(TAG, "Detected amplitude: $amplitude dB")
                     wavRecorder?.stopRecording()
-                    if (amplitude > AMPLITUDE_THRESHOLD) {
+                    if (amplitude > AMPLITUDE_DB_THRESHOLD) {
                         Log.d(
                             TAG,
                             "Start recording for subsequent detection"
@@ -138,7 +138,7 @@ class RecordingService : Service() {
                             }, RECORDING_FOR_ML_SECONDS, TimeUnit.SECONDS
                         )
                     }
-                }, CHECK_AMPLITUDE_SECONDS, TimeUnit.SECONDS
+                }, CHECK_AMPLITUDE_DURATION, TimeUnit.SECONDS
             )
         }
     }
